eba2a149edba42c0afd9eb6448e2ef8a45bb6442
jeshof28-oss <jeshof28@gmail.com>
2026-01-22 18:57:12 +0000

    2d list

---
+++ Matrix.py
@@ -0,0 +1,25 @@
+matrix = [[1, 2, 3],[4,5,6],[7,8,9]]
+print(matrix)
+print(len(matrix))
+print(len(matrix[0]))
+for i in range(0, len(matrix)):
+    for j in range(0, len(matrix)):
+        print(matrix[i][j], end=" ")
+    print("\n")
+
+rows=int(input("Enter the number of rows - "))
+col=int(input("Enter the number of columns - "))
+
+Matrix = []
+
+for i in range(rows):
+    temp=[]
+    for j in range(col):
+        val = int(input("Enter the value - "))
+        temp.append(val)
+    Matrix.append(temp)
+
+for i in range(0, len(Matrix)):
+    for j in range(0, len(Matrix)):
+        print(Matrix[i][j], end=" ")
+    print("\n")
