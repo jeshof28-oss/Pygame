2cace6b11659384eca08c742b30f1419ac583173
Jeshof Jenith <jeshof28@gmail.com>
2026-02-12 00:09:48 +0000

    Homework added

---
+++ Tuples HW.py
@@ -0,0 +1,17 @@
+records = []
+
+for i in range(2):
+    print("\nEnter details for Group {}:".format(i+1))
+    
+    group_name = input("Enter group name: ")
+    group_size = int(input("Enter size of the group: "))
+    date_of_competition = input("Enter date of the competition: ")
+    venue = input("Enter venue: ")
+    medal_type = input("Enter type of medal: ")
+
+    record = (group_name, group_size, date_of_competition, venue, medal_type)
+    records.append(record)
+
+print("\n"+"All Group Records:")
+for record in records:
+    print(record)
