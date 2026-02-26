31c8163e92e9abd67d158b380c1167c049bb5e13
Jeshof Jenith <jeshof28@gmail.com>
2026-02-26 18:51:11 +0000

    recycle

---
+++ recycle.py
@@ -0,0 +1,18 @@
+import pgzrun
+import random
+
+FONT_option = (255, 255, 255)
+WIDTH = 800
+HEIGHT = 600
+center_x=WIDTH / 2
+center_y=HEIGHT / 2
+CENTRE = (center_x , center_y)
+final_level=6
+start_speed=6
+items=["bag", "battery", "bottle", "chips"]
+
+game_over= False
+game_complete= True
+current_level= 1
+items = []
+animations = []
