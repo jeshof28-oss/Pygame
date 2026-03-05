36e247d7b905339540c7c4f40802174ca6407e97
Jeshof Jenith <jeshof28@gmail.com>
2026-03-05 18:52:11 +0000

    update recycle

--- recycle.py
+++ recycle.py
@@ -16,3 +16,57 @@ game_complete= True
 current_level= 1
 items = []
 animations = []
+
+def draw():
+    global items, current_level, game_over, game_complete
+    screen.blit("bg", (0,0))
+    if game_over:
+        display_message("Game Over, try again?")
+    if game_complete:
+        display_message("Game Complete!, Well Done")
+    else:
+        for item in items:
+            item.draw()
+
+def update():
+    global items
+    if len(items) == 0:
+        items = make_items(current_level)
+
+def make_items(num_of_extra):
+    items_to_make = get_option_to_create(num_of_extra)
+    new_items = create_items(items_to_make)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_option_to_create(num_of_extra):
+    items_to_make=[]
+    for i in range(0, num_of_extra):
+        random_select=random.choice(items)
+        items_to_make.append(random_select)
+    return items_to_make
+
+def create_items():
+    new_items=[]
+    for i in items_to_make:
+        new_item=Actor(option + "img")
+        new_items.append(new_item)
+    return new_items
+
+def layout_items(items_to_lay):
+    number_of_gaps = len(items_to_lay) + 1
+    gap_size= WIDTH / number_of_gaps
+    random.shuffle(items_to_lay)
+    for index, item in enumerate(items_to_lay):
+        new_x_pos=index*gap_size
+        item.x=new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for i in items_to_animate:
+        duration = start_speed - current_level
+        i.anchor = ("center", "bottom")
+        animation = animate(i, duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+    
